37d452e028a847c9a4f13cf045d85d9ec5e99f8a
ninzero <ninzero@DESKTOP-DCNQQJA.localdomain>
2020-03-16 14:03:55 +0000

    Reconstruct version 1.0

--- Version1.0.py
+++ Version1.0.py
@@ -5,20 +5,24 @@ from functools import reduce
 
 def find_black_hole_number(num):
 
-    def sort_digits(temp, biggest=True):  # To list the number, sort the list and turn it back into a number again.
-        return reduce(lambda y, z: y*10+z, sorted(map(lambda x: int(x), list(str(temp))), reverse=biggest))
+    def list2int(a: list) -> int:
+        return reduce(lambda x, y: x*10+y, a)
 
-    path = [num]  # If we find a number in path, it means a black hole number(group) has occur.
-    while True:  # Run until find black hole number
-        largest_version = sort_digits(num)
-        smallest_version = sort_digits(num, biggest=False)
-        num = largest_version-smallest_version
+    def int2list(a: int) -> list:
+        a = list(str(a))
+        return map(lambda x: int(x), a)
+
+    path = [num]  # If we find a repeated number in this list, it means a black hole number(group) has occur.
+    while True:
+        largest_combination = list2int(sorted(int2list(num), reverse=True))
+        smallest_combination = list2int(sorted(int2list(num)))
+        num = largest_combination - smallest_combination
         if not num:  # Deal with numbers such as 1111.
             return 0
-        elif num < start:  # Four-digit number 1000, f(1000)=1000-1=999, not a four-digit number anymore
-            num = num*10
+        elif num < start:  # For example 1000, f(1000)=1000-1=999, not a four-digit number anymore.
+            num *= 10
         elif num in path:
-            return num  # We have find a black hole number(group), at the same time break the loop.
+            return num  # We have found a black hole number(group).
         else:
             path.append(num)
 
