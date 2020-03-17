3667c8cdc068d9e99c17f2cbac04d52bdf8b5f28
ninzero <ninzero@DESKTOP-DCNQQJA.localdomain>
2020-03-17 23:34:49 +0000

    Reconstruct code and remove time.clock() function.

--- Version1.1.py
+++ Version1.1.py
@@ -1,44 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from CommonFunction import list2int, int2list
+from CommonFunction import format_black_holes
 from functools import reduce
 import time
 import random
 
 
 def find_black_hole_number(num):
-
-    def sort_digits(temp, biggest=True):  # To list the number, sort the list and turn it back into a number again.
-        return reduce(lambda y, z: y*10+z, sorted(map(lambda x: int(x), list(str(temp))), reverse=biggest))
-
-    path = [num]  # If we find a number in path, it means a black hole number(group) has occur.
-    while True:  # Run until find black hole number
-        largest_version = sort_digits(num)
-        smallest_version = sort_digits(num, biggest=False)
-        num = largest_version-smallest_version
-        if not num:
+    path = [num]  # If we find a repeated number in this list, it means a black hole number(group) has occur.
+    while True:
+        maximum = list2int(sorted(int2list(num), reverse=True))
+        minimum = list2int(sorted(int2list(num)))
+        num = maximum - minimum
+        if not num:  # Deal with numbers such as 1111.
             return 0
-        elif num < start:
-            num = num*10
+        elif num < start:  # For example 1000, f(1000)=1000-1=999, not a four-digit number anymore.
+            num *= 10
         elif num in path:
-            return num  # We have find a black hole number(group), at the same time break the loop.
+            return num  # We have found a black hole number(group).
         else:
             path.append(num)
 
 
 if __name__ == '__main__':
     digits = int(input('Please enter the number of digits:'))
-    time.clock()
     start = 10**(digits-1)  # For example, the start of three-digit number is 10**(3-1)=100
     end = 10**digits-1  # For example, the end of three-digit number is 10**3-1=999
-    blackHoleNumbers = set()  # Use set means we don't have to deduplicate later.
+    black_hole_numbers = set()  # Use set means we don't have to deduplicate later.
+    start_time = time.process_time()
     for x in range(100000):
         number = random.randint(start, end)
-        blackHoleNumbers.add(find_black_hole_number(number))
+        black_hole_numbers.add(find_black_hole_number(number))
     try:
-        blackHoleNumbers.remove(0)  # Remove "fake" black hole number 0 caused by numbers with no different digits.
+        black_hole_numbers.remove(0)  # Remove "fake" black hole number 0 caused by numbers with no different digits.
     except KeyError:
         pass
-    timeSpend = time.clock()
-    timeEstimate = timeSpend/100000*(end-start+1)
-    print(timeSpend)
-    print(timeEstimate)
+    stop_time = time.process_time()
+    time_spent = stop_time - start_time
+    estimate_time = time_spent/100000*(end-start+1)
+    print(time_spent)
+    print(estimate_time)
